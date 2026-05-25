7fbefbd855d88cf7f27cffae278aba33336f996c
Ahmed Anwer <104807894+realAhmedAnwer@users.noreply.github.com>
2026-05-25 23:09:15 +0300

    Add IsDeleted property to BaseEntity for soft deletes

--- Gymly.Domain/Entities/BaseEntity.cs
+++ Gymly.Domain/Entities/BaseEntity.cs
@@ -3,6 +3,7 @@
 public abstract class BaseEntity
 {
     public int Id { get; set; }
+    public bool IsDeleted { get; set; } = false;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public string? CreatedBy { get; set; }
     public DateTime? UpdatedAt { get; set; }
